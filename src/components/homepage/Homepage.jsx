6594c812006658ef0fcc9f289dd3d8835d2b53e5
M4nu3l4 <142220899+M4nu3l4@users.noreply.github.com>
2025-01-28 13:13:13 +0100

    c

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,9 +1,27 @@
-import NewsHome from './NewsHome'
+import  { useState } from "react";
+import NewsHome from "../homepage/NewsHome"; 
 
-const Homepage = ()=>{
-    return(
-        <NewsHome/>
-    )
+const Homepage = () => {
+  const [visiblePosts, setVisiblePosts] = useState(6); 
 
-}
-export default Homepage
+  const handleShowMore = () => {
+    setVisiblePosts((prev) => prev + 6); 
+  };
+
+  return (
+    <div>
+      
+      <NewsHome visiblePosts={visiblePosts} showInput={false} />
+      <div className="text-center mt-4">
+        <button className="btn btn-primary" onClick={handleShowMore}>
+          Show More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
+
+
+

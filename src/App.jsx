eac8e3c81f42e01a4d595ad1799c6ccabe0ab8b5
M4nu3l4 <142220899+M4nu3l4@users.noreply.github.com>
2025-01-28 11:46:21 +0100

    28/01 home

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./style.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./components/profile/ProfilePage";
 import FooterSection from "./components/Footer";
+import NewsHome from "./components/NewsHome"
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProfilePage />} />
       </Routes>
+      <NewsHome />
       <FooterSection />
     </BrowserRouter>
   )

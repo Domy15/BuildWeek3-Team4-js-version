fc563dcbf579d130668fe1c290a7afaf4b212c85
Ninfa-C <ninfa_95@hotmail.it>
2025-01-29 11:57:09 +0100

    fix bug :(

--- src/components/profile/AsideSection.jsx
+++ src/components/profile/AsideSection.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { PersonFillAdd, PersonFillDash } from "react-bootstrap-icons";
 import CompanyAside from "./CompanyAside";
 
-const AsideSection = () => {
+const AsideSection = ({param}) => {
   const users = useSelector((state) => state.friends);
   const follow = useSelector((state) => state.interaction.favourites);
   const dispatch = useDispatch();
@@ -20,7 +20,7 @@ const AsideSection = () => {
 
   useEffect(() => {
     dispatch(ProfilesApi(url, token, randomProfiles));
-  }, []);
+  }, [param]);
 
   return (
     <div>
@@ -59,7 +59,6 @@ const AsideSection = () => {
                         <Link
                           to={`/profile/${u._id}`}
                           className="text-decoration-none text-black"
-                          onClick={dispatch({type: 'UPDATE'})}
                         >
                           {`${u.name} ${u.surname}`}
                         </Link>

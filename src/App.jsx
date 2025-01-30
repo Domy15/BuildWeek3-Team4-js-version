c0302a1b92bee74011614b47761391a7c0ab91cf
Domy15 <domenico2016dellaventura@gmail.com>
2025-01-30 12:43:25 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
 import "./style.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
---
+++ src/components/homepage/Carousel.jsx
@@ -0,0 +1,59 @@
+import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { style, onClick } = props;
+  return (
+    <ChevronLeft
+    style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { style, onClick } = props;
+  return (
+    <ChevronRight
+    style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
+
+const Carousel = () => {
+  const bntOption = [
+    "Mi piace",
+    "Geniale",
+    "Suggerimenti Utili",
+    "Molto Utile",
+    "Interessante",
+    "Ottimo consiglio",
+    "Concordo",
+  ];
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    variableWidth: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+    <Slider {...settings} className="my-3">
+      {bntOption.map((btn, i) => (
+        <div
+          key={i}
+          className="btnSectionComm btn bg-transparent border border-1 border-black rounded-5 text-center px-4 carouselComment"
+        >
+          {btn}
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default Carousel;

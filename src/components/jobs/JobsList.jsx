9f3d09c09a814678673c80502afaa284b2133c54
Domy15 <domenico2016dellaventura@gmail.com>
2025-01-31 11:06:11 +0100

    commit

--- src/components/jobs/JobsList.jsx
+++ src/components/jobs/JobsList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useLocation } from "react-router-dom";

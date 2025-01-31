3bec4dfa0d56052a24a4d1974c0271b04f1e2422
matildepetrilli <matilde.petrilli10@gmail.com>
2025-01-31 01:15:53 +0100

    commit

--- src/components/search/SearchJobs.jsx
+++ src/components/search/SearchJobs.jsx
@@ -1,32 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
+import { Search } from "react-bootstrap-icons";
+import { useMediaQuery } from "react-responsive";
+import { useNavigate } from "react-router-dom";
 
 
 const SearchJobs = () => {
   const [query, setQuery] = useState("");
-  const [jobs, setJobs] = useState([]);
+  const navigate = useNavigate();
+  const [show,setShow]=useState(false);
 
-  const baseEndpoint =
-    "https://strive-benchmark.herokuapp.com/api/jobs?search=";
+const responsive=useMediaQuery({query:"(max-width:769px)"})
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(baseEndpoint + query + "&limit=20");
-      if (response.ok) {
-        const { data } = await response.json();
-        setJobs(data);
-      } else {
-        alert("Errore nella chiamata");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const handleChange = (e) => {
+    setQuery(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/Jobs", { state: query });
+  };
+  
+
+useEffect(()=>{
+  if(responsive){
+    setShow(false)
+  }else{setShow(true)}
+},[responsive])
+
 
   return (
     <>
-      
+        <Form onSubmit={handleSubmit} className="d-flex align-items-center">
+          <Search onClick={()=>setShow(!show)} size={25} className="mx-3" />
+          {show && <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2 d-flex "
+            aria-label="Search"
+            value={query}
+            onChange={handleChange}
+          />}
+        </Form>     
     </>
   );
 };
